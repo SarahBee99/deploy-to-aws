76cf0e6e577e468a48881c49e6254fb74ba2f6e8
Sarah Rashed <sarahbelal.99.1a19@gmail.com>
2026-06-10 19:31:02 +0300

    change to bucket name

--- variables.tf
+++ variables.tf
@@ -13,5 +13,5 @@ variable "profile" {
 variable "bucket_prefix" {
   description = "Prefix for S3 bucket name (must be globally unique)"
   type        = string
-  default     = "myapp-web-sarah"
+  default     = "my-second-web-app"
 }
